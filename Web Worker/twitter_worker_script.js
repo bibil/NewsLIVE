bdc04ba8a14579f19e224978a462cd084d20ec54
Kareem <lkareem.alil@gmail.com>
2013-12-08 17:32:26 +0200

    deleting unused files

--- Web Worker/twitter_worker_script.js
+++
@@ -1,7 +0,0 @@
-var w;
-
-if(typeof(Worker)!=="undefined"){
-	w = new Worker("WebWorker.js");
-	alert(w);
-	w.onmessage = function (event){document.getElementById("result").innerHTML = event.data};
-}
