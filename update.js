2ea10056b62d525d340ccaac7642a218f3b53a73
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2013-12-09 01:07:12 +0200

    Initial xml code

--- update.js
+++ update.js
@@ -1,8 +1,11 @@
 self.addEventListener('message',  function(event){
     var currPrime = event.data, nextPrime;
     setInterval( function(){
-    
-    var message = "lol";
+    xmlhttp=new XMLHttpRequest();
+    xmlhttp.open("GET","http://rss.nytimes.com/services/xml/rss/nyt/MiddleEast.xml",false);
+    xmlhttp.send();
+    xmlDoc=xmlhttp.responseXML; 
+    message =xmlDoc.getElementsByTagName("item")[0].childNodes[0].nodeValue;
     postMessage(message);   
     }, 500);
 });
