b8f664423dfbd046b756aa85d24081ee618891a6
muhammed <muhammedabuali@gmail.com>
2013-12-09 17:29:43 +0200

    rss feed with jquery -- no rss --

--- local.js
+++ local.js
@@ -1,17 +1,5 @@
-var count = 0;
-window.setInterval(function(){
-    count++;
-    if (count == 4) {
-        document.getElementById("news").className = "fade-out";
-        btnElement = "Fade In";
-        count = -1;
-    }
-    else if (count == 0){
-        document.getElementById("news").className = "fade-in";
-        btnElement = "Fade Out";
-    }
-}, 1500);
-var worker = new Worker('update.js');
+
+var worker = new Worker('work.js');
 var current = "";
 worker.postMessage(current);
 worker.addEventListener('message', function(event){
